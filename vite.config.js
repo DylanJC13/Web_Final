8e2b493f3b151559493b8f98f790610708d3eeca
DylanJC13 <dylanjc1290@gmail.com>
2025-05-11 12:41:09 -0500

    Correcion GitPages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: 'https://github.com/DylanJC13/Web2_Final.git', // Reemplaza con tu repo de GitHub
+  base: '/Web_Final/', // Reemplaza con tu repo de GitHub
   plugins: [react()],
 });
